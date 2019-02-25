6f5e79124718c399cf160f84709896c11b57806c
Changsoo Jung <cjung5@mail.csuchico.edu>
2019-02-24 19:46:15 -0800

    add group meeting work

--- StudyTogetherTest/app/src/main/java/com/example/studytogether/studytogethertest/MainActivity.java
+++ StudyTogetherTest/app/src/main/java/com/example/studytogether/studytogethertest/MainActivity.java
@@ -10,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.studytogether.studytogethertest.model.User;
+import com.example.studytogether.studytogethertest.model.Group;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
