35472a64015f648926490fbdc96fcbae758aab6b
Changsoo Jung <cjung5@mail.csuchico.edu>
2019-04-07 13:00:00 -0700

    add google sign-in system

--- app/src/main/java/com/studytogether/studytogether/Activities/LoginActivity.java
+++ app/src/main/java/com/studytogether/studytogether/Activities/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,23 +12,37 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.squareup.picasso.Picasso;
 import com.studytogether.studytogether.R;
+
 
 // Login Activity
 public class LoginActivity extends AppCompatActivity {
 
+
+    static final int GOOGLE_SIGN_IN = 123;
+
     // Items
-    private EditText userMail,userPassword;
-    private Button btnLogin;
+    private EditText userMail, userPassword;
+    private Button btnLogin, btnGoogleLogin;
     private ProgressBar loginProgress;
     private FirebaseAuth mAuth;
     private Intent HomeActivity;
     private ImageView loginPhoto;
+
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +55,7 @@ public class LoginActivity extends AppCompatActivity {
         userPassword = findViewById(R.id.login_password);
         btnLogin = findViewById(R.id.login_btn);
         loginProgress = findViewById(R.id.login_progress);
+        btnGoogleLogin = findViewById(R.id.google_login_btn);
 
         // Firebase authorization
         mAuth = FirebaseAuth.getInstance();
@@ -63,6 +79,14 @@ public class LoginActivity extends AppCompatActivity {
 
         // Set the progress-button as invisible mode
         loginProgress.setVisibility(View.INVISIBLE);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
         // Listen the login-button
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -89,6 +113,49 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
+
+        btnGoogleLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showMessage("google login btn clicked");
+                SignInGoogle();
+            }
+        });
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        showMessage("firebaseAuthWithGoogle");
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        showMessage("User logged in successfully");
+                        loginProgress.setVisibility(View.INVISIBLE);
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        updateUIGoogle(user);
+                    } else {
+                        showMessage("Could not log in user");
+                        loginProgress.setVisibility(View.INVISIBLE);
+                        updateUIGoogle(null);
+                    }
+                });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        showMessage("onActivityResult");
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                showMessage("try");
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                showMessage("error");
+            }
+        }
     }
 
     // Sign in
@@ -116,11 +183,32 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    public void SignInGoogle() {
+        showMessage("SignInGoogle");
+        loginProgress.setVisibility(View.VISIBLE);
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
+    }
+
     // Update UI: replace the activity into Home activity
     private void updateUI() {
         startActivity(HomeActivity);
         // Finish this activity
         finish();
+    }
+
+    private void updateUIGoogle(FirebaseUser user) {
+        if (user != null) {
+            showMessage("User logged in successfully");
+
+            String photo = String.valueOf(user.getPhotoUrl());
+            Picasso.with(LoginActivity.this).load(photo).into(loginPhoto);
+
+            startActivity(HomeActivity);
+            finish();
+        } else {
+            showMessage("Could not log in user");
+        }
     }
 
     // Show message to the user
@@ -131,10 +219,13 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        FirebaseUser user = mAuth.getCurrentUser();
         // TODO: Block the non-authorized user
-        if(user != null) {
-            updateUI();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if( currentUser != null) {
+            updateUIGoogle(currentUser);
+        } else {
+            showMessage("current user is null");
+            SignInGoogle();
         }
     }
 }
